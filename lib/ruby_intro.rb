a78e5dc9986b13f1d3a949c40892fbfb223ccee1
cherryball <greencherryball@gmail.com>
2015-10-26 01:05:46 +0000

    Finish part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,44 @@
 
 # Part 1
 
-def sum arr
+def sum(arr)
   # YOUR CODE HERE
+   total = 0
+   arr.each { |x| total += x }  unless arr.empty?
+   return total
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  total = 0
+  if arr.empty?
+    total = 0
+  elsif arr.length == 1
+    total = arr[0]
+  else
+   arr.sort! { |x, y| y <=> x } 
+#   arr.each { |x| puts x }
+   (0..1).each { |x| total += arr[x] }
+  end
+  
+  return total
+  
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.empty? || arr.length == 1
+    return false
+  else
+    arr.each do |x|
+      arr.each { |y| 
+   #      if x != y && x + y == n
+    #     puts "#{x} + #{y} = #{total}"
+         return true if x != y && x + y == n
+       }
+    end
+  end
+  return false
 end
 
 # Part 2
@@ -33,3 +61,14 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+=begin
+arr = [5, 7, 3, 4, 5]
+total = max_2_sum(arr)
+puts total
+
+
+puts "sum_to_n? test"
+arr = [-1,-2,3,4,6,-8]
+puts sum_to_n? arr, 12
+=end
